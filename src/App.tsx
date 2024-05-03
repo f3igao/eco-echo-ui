3ee073460f9076b55e7c86784659cec2db882901
fei <f3igao@icloud.com>
2024-05-03 00:47:42 -0400

    remove app css

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,4 @@
-import './App.css';
-
 function App() {
-  // const [count, setCount] = useState(0);
-
-  // fetch('http://127.0.0.1:5000/activities')
-  //   .then((response) => response.json())
-  //   .then((data) => {
-  //     console.log('data', data);
-  //   })
-  //   .catch((error) => {
-  //     console.error('Error fetching data:', error);
-  //   });
-
   return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
 }
 
